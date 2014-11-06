1f62ca3f80035d477632c5fe3e0876627b78178d
bhernandezsouto <bhernandezsouto@eniac31.danielcastelao.org>
2014-11-06 10:33:38 +0100

    boletin 7

---
+++ src/boletin_7_5/Boletin_7_5.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_7_5;
+
+public class Boletin_7_5 {
+
+    public static void main(String[] args) {
+       Compara op = new Compara();
+       op.esMayor();
+    }
+    
+}
---
+++ src/boletin_7_5/Compara.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_7_5;
+import javax.swing.JOptionPane;
+public class Compara {
+    private double a;
+    private double b;
+    private double c;
+    
+    public Compara(){
+        String respuesta = JOptionPane.showInputDialog("Introduce el primer numero");
+        a = Double.parseDouble(respuesta);
+        respuesta = JOptionPane.showInputDialog("Introduce el segundo numero");
+        b = Double.parseDouble(respuesta);
+        respuesta = JOptionPane.showInputDialog("Introduce el tercer numero");
+        c = Double.parseDouble(respuesta);
+    }
+    public void esMayor (){
+        int p = 0;
+        if ((a>b)&&(a>c)){
+            JOptionPane.showMessageDialog(null, "El numero mayor es el 1ª numero: " + a);
+            p=1;
+        }
+        if ((b>a)&&(b>c)){
+            JOptionPane.showMessageDialog(null, "El numero mayor es el 2ª numero: " + b);
+            p=1;
+        }
+        if ((c>a)&&(c>b)){
+            JOptionPane.showMessageDialog(null, "El numero mayor es el 3ª numero: " + c);
+            p=1;
+        }
+        if (p == 0){
+            JOptionPane.showMessageDialog(null, "los tres numeros son iguales");
+        }        
+    }
+}
